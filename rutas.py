56d764028246a83bfe97a1117e7e3851056b4bcf
Guillermo Vaya <guillermo.vaya@gigas.com>
2014-04-02 08:56:38 +0200

    wip

--- rutas.py
+++ rutas.py
@@ -1,18 +1,44 @@
 import hy
 import model
-from common import app
+from common import app, lexer_list
 from flask import request, render_template, flash, redirect, url_for, abort
+from slugify import slugify
+
 
 @app.route("/")
 def get_index():
     return render_template("home.html")
 
-#TODO: add filters by user
-#TODO: add pagination
+
+def show_pasta_list(pasta_list):
+    return render_template("pasta-list.html",
+                           pasta_list=pasta_list,
+                           sauce_list=lexer_list)
+
+
 @app.route("/list/")
 def list_pastes():
-    pasta_list = model.get_some_pasta()
-    return render_template("pasta-list.html", pasta_list=pasta_list)
+    return show_pasta_list(model.get_some_pasta())
+
+
+@app.route("/list/<int:page>")
+def list_pastes_page(page):
+    """
+    traverse pagination of results
+    Arguments:
+    - `page`:
+    """
+    return show_pasta_list(model.get_some_pasta(start_from=page))
+
+
+@app.route("/list/<user>")
+def user_pastes(user):
+    """
+    List pastes of a user
+    Arguments:
+    - `user`:
+    """
+    return show_pasta_list(model.get_user_pasta (user))
 
 @app.route("/c/<user>/<key>/")
 def get_pasta(user, key):
@@ -20,16 +46,22 @@ def get_pasta(user, key):
     if pasta is None:
         abort(404)
     else:
-        return render_template("pasta.html", pasta=pasta["code"], user=user, key=key)
+        return render_template("pasta.html",
+                               pasta=pasta["code"], 
+                               user=user,
+                               key=key)
 
 @app.route("/c/<user>/<key>/<lexer>/")
 def get_pasta_with_sauce(user, key, lexer):
     pasta = model.get_pasta(user, key)
     if pasta is None:
         abort(404)
-    else:
-        with_sauce = model.pasta_with_sauce(pasta, lexer)
-        return render_template("pasta-sauce.html", pasta=with_sauce["code"], user=user, key=key)
+
+    with_sauce = model.pasta_with_sauce(pasta, lexer)
+    return render_template("pasta-sauce.html", 
+                           pasta=with_sauce["code"], 
+                           user=user, 
+                           key=key)
     
 
 @app.route("/new/", methods=["GET", "POST"])
@@ -37,13 +69,13 @@ def create_new():
     app.logger.debug("Form keys: " + ", ".join(request.form.keys()))
     if request.method=="GET":
         return render_template("new.html", 
-                        user=request.args.get("user", ""),
-                        key=request.args.get("key", ""),
-                        code=request.args.get("code", ""))
+                               user=slugify(request.args.get("user", "")),
+                               key=slugify(request.args.get("key", "")),
+                               code=request.args.get("code", ""))
     else:
-        user = request.form.get("user", "Anonymous")
+        user = slugify(request.form.get("user", "Anonymous"))
         try:
-            key =  request.form.get("key", model.gen_key(user))
+            key =  slugify(request.form.get("key", model.gen_key(user)))
         except KeyError as e:
             app.logger.error("Too many retries to generate a key")
             abort(500)
@@ -58,3 +90,7 @@ def create_new():
         else:
             model.add_pasta(user, key, code)
             return redirect(url_for("get_pasta", user=user, key=key))
+
+@app.route("/c/<user>/<key>/<lexer>/")
+def get_pasta_with_sauce(user, key, lexer):
+    
