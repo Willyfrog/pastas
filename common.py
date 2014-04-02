74a9fb1c82259c78f6f6f7469986793509c09223
Guillermo Vaya <guillermo.vaya@gigas.com>
2014-04-02 08:56:12 +0200

    Translation core->common

--- common.py
+++ common.py
@@ -2,6 +2,18 @@ import os
 from flask import Flask
 import dataset
 
-db = dataset.connect("sqlite:///pastas.db")
+with open("config.json") as jfile:
+    try:
+        json_config = json.load(jfile)
+    except:
+        print ("no config loaded from config.json")
+
+
 app = Flask("__main__")
-app.secret_key = "replacemeforsomethingunique"
+app.secret_key = json_config.get("secretkey", "replacemeforsomethingunique")
+
+db = dataset.connect(json_config.get("dbstring", "sqlite:///memory"))
+
+PPP = json_config.get("PPP", 20)
+
+lexer_list = json_config.get("lexer_list", [("python", "Python") ( "raw", "Raw") ( "clj", "Clojure") ( "php", "PHP") ( "html", "HTML")])
